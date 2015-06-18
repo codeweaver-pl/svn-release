b7bfea156c983aba1257af0cbb9630a2a1b9a76e
Tomasz Zabłocki <waxxfetish@gmail.com>
2015-06-18 05:53:46 +0200

    set version step

--- bin/svn-release2.js
+++ bin/svn-release2.js
@@ -2,18 +2,6 @@
 "use strict";
 
 var app = require('../lib/svn-release2'),
-  mkdirp = require('mkdirp');
 
-mkdirp('./dist', function (error) {
-  if (error) {
-    console.error(error);
-  } else {
-    app.performTag({}, function (err, info) {
-      if (err) {
-        console.error('error occurred while performing svn release', err);
-      } else {
-        console.log(info);
-      }
-    });
-  }
-});
+
+    app.branch();
