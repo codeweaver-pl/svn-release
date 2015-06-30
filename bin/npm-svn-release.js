4bc11b2928df48357c1cef9f0398b4080036856d
Tomasz Zabłocki <waxxfetish@gmail.com>
2015-06-30 12:30:14 +0200

    first functional dist release

--- bin/npm-svn-release.js
+++ bin/npm-svn-release.js
@@ -1,11 +1,36 @@
 #!/usr/bin/env node
 "use strict";
 
-var SvnRelease     = require('../lib/svn-release'),
-    SvnDefaults    = require('../lib/svn-defaults'),
+var _           = require('lodash'),
+    log         = require('npmlog'),
+    SvnRelease  = require('../lib/svn-release'),
+    SvnDefaults = require('../lib/svn-defaults'),
 
-    lifecycleEvent = process.env.npm_lifecycle_event,
-    svnDefaults    = new SvnDefaults(process.env.npm_package_version),
-    svnRelease     = new SvnRelease('.');
+    npmCtx      = parseNpmCtx(),
 
-    svnRelease.release(svnDefaults.releaseOptions);
+    svnRelease  = new SvnRelease('.'),
+    svnDefaults = new SvnDefaults(npmCtx.packageVersion);
+
+log['enableColor']();
+log.stream      = process.stdout;
+
+log.info('svn-release', 'release version %s', npmCtx.packageVersion);
+
+svnRelease.release(svnDefaults.releaseOptions);
+
+function parseNpmCtx() {
+
+  return _.chain(process.env)
+    .pick(npmProperty)
+    .reduce(toSvnReleaseProperty, {})
+    .value();
+
+  function npmProperty(value, key) {
+    return _.startsWith(key, 'npm');
+  }
+
+  function toSvnReleaseProperty(memo, value, key) {
+    memo[_.camelCase(key.slice(4))] = value;
+    return memo;
+  }
+}
