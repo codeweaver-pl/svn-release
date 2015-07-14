66daa4189aa87d413a57bcfd83f15cab8a6d25b2
Tomasz Zabłocki <waxxfetish@gmail.com>
2015-07-14 12:42:49 +0200

    cleanup

--- bin/svn-release.js
+++ bin/svn-release.js
@@ -5,14 +5,21 @@
  * @typedef {*} Mixed
  */
 
-var program       = require('commander'),
+var URI_REGEX     = /^(((?:(?:[a-z0-9-._~!$&'()*+,;=:]|%[0-9A-F]{2})*)@)?((?:[a-z0-9-._~!$&'()*+,;=]|%[0-9A-F]{2})*)(:(?:\d*))?(\/(?:[a-z0-9-._~!$&'()*+,;=:@\/]|%[0-9A-F]{2})*)?|([a-z0-9+.-]+:)(\/?(?:[a-z0-9-._~!$&'()*+,;=:@]|%[0-9A-F]{2})+(?:[a-z0-9-._~!$&'()*+,;=:@\/]|%[0-9A-F]{2})*)?)(\?(?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*)?(#(?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*)?$/i,
+    BOOLEAN_REGEX = /^((true)|(false))$/i,
+    NAME_REGEX    = /^[\w-]+$/i,
+
+    semverRegex   = require('semver-regex'),
+    program       = require('commander'),
     fs            = require('fs'),
-    SvnReleaseCli = require('./svn-release-cli'),
     SvnRelease    = require('../lib/svn-release'),
+    logger        = require('../lib/svn-logger'),
 
     packageJson   = JSON.parse(fs.readFileSync('./package.json', 'utf8')),
     svnDefaults   = new SvnRelease.SvnDefaults(packageJson.version),
-    svnRelease    = new SvnRelease('.');
+    svnRelease    = new SvnRelease('.'),
+    action        = null,
+    actionOptions = null;
 
 program
   .usage('<cmd> [options...]')
@@ -20,7 +27,7 @@ program
   .allowUnknownOption(false)
   .option('-I, --interactive',
           'perform branch or release tasks in interactive mode',
-          SvnReleaseCli.boolean,
+          BOOLEAN_REGEX,
           false);
 
 program
@@ -28,61 +35,72 @@ program
   .description('create new branch, based on current working copy')
   .option('-n, --branch-name [name]',
           'override default branch name',
-          SvnReleaseCli.name,
+          NAME_REGEX,
           svnDefaults.branchName)
   .option('-b, --branch-version [version]',
           'override default branch version',
-          SvnReleaseCli.version,
+          semverRegex,
           svnDefaults.branchVersion)
   .option('-N, --next-version [version]',
           'override next version',
-          SvnReleaseCli.version,
+          semverRegex,
           svnDefaults.postBranchVersion)
   .option('-B, --branches [path]',
           'relative path to branches',
-          SvnReleaseCli.uri,
+          URI_REGEX,
           'branches')
-  .action(doBranch);
+  .action(onBranch);
 
 program
   .command('release')
   .description('create new release, based on current working copy')
   .option('-n, --release-name [name]',
           'overrides default release version',
-          SvnReleaseCli.name,
+          NAME_REGEX,
           svnDefaults.releaseName)
   .option('-r, --release-version [version]',
           'overrides default release version',
-          SvnReleaseCli.version,
+          semverRegex,
           svnDefaults.releaseVersion)
   .option('-N, --next-version [version]',
           'overrides next version',
-          SvnReleaseCli.version,
+          semverRegex,
           svnDefaults.postReleaseVersion)
   .option('-d, --dist [dist-folder]',
           'include distribution folder on release',
-          SvnReleaseCli.name,
+          NAME_REGEX,
           null)
   .option('-R, --releases [path]',
           'relative path to releases (tags)',
-          SvnReleaseCli.uri,
+          URI_REGEX,
           'tags')
-  .action(doRelease);
+  .action(onRelease);
 
 program.parse(process.argv);
+
+svnRelease[action](actionOptions)
+  .done(onFulfilled, onRejected);
 
 if (!program.args.length) {
   program.outputHelp();
 }
 
-function doBranch(options) {
-  svnRelease.branch(
-    SvnReleaseCli.branchOptions(options)
-  ).done(console.log, console.err);
+function onBranch(options) {
+  actionOptions = svnDefaults.branchOptions(options);
+  action        = 'branch';
 }
 
-function doRelease(options) {
-  svnRelease.release(
-    SvnReleaseCli.releaseOptions(options)
-  ).done(console.log, console.err);
+function onRelease(options) {
+  actionOptions = svnDefaults.releaseOptions(options);
+  action        = 'release';
+}
+
+function onFulfilled(value) {
+  logger.info('execution successful');
+  logger.debug('last call result:', JSON.stringify(value));
+}
+
+function onRejected(reason) {
+  logger.error('execution failed');
+  logger.warn('error:', reason);
 }
