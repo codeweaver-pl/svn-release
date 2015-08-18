53181f69cc3986bae70c34508a115938b1b656c4
Tomasz Zabłocki <waxxfetish@gmail.com>
2015-08-18 18:39:20 +0200

    SvnRelease tests

--- test/svn-release.js
+++ test/svn-release.js
@@ -1,15 +1,48 @@
 'use strict';
 
-var expect     = require('chai').expect,
+var _              = require('lodash'),
+    chai           = require('chai'),
+    chaiAsPromised = require("chai-as-promised"),
+    Q              = require('q'),
+    SvnRelease     = require('../lib/svn-release'),
+    expect         = chai.expect,
+    svnRelease,
+    svnHandle;
 
-    SvnRelease = require('../lib/svn-release'),
-    svnRelease = new SvnRelease('.');
+chai.use(chaiAsPromised);
 
-describe('SvnDefaults', function () {
+describe('SvnRelease', function () {
 
-  describe('#()', function () {
-    it('should ', function () {
-      expect(true).to.equal(true);
+  describe('#release()', function () {
+
+    var releaseOpts = {
+      releases:       'tags',
+      releaseName:    '1.1.2',
+      nextVersion:    '1.1.3',
+      releaseVersion: '1.1.2',
+      dist:           'dist'
+    };
+
+    beforeEach(function () {
+
+      svnHandle = {
+        status: function (cb) {
+          cb(null, [
+            {status: 'M', path: 'a-modified-file'},
+            {status: 'D', path: 'a-deleted-file'}
+          ]);
+        }
+      };
+
+      svnRelease = new SvnRelease('.', svnHandle);
+    });
+
+    it('should crash ', function () {
+
+      return expect(svnRelease.release(releaseOpts)).to.eventually.be.rejected
+        .then(function (err) {
+                expect(err).to.be.an.instanceof(Error);
+              });
     });
   });
 });
